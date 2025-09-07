a6167c9396dee4dd771c083b81cc2a696c70eea6
pushpanjalipg <pushpanjalipg700@gmail.com>
2025-09-07 23:06:47 +0530

    after add vite

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 const apiUrl = process.env.REACT_APP_API_URL;
-// const apiUrl =import.meta.env.VITE_API_URL 
-console.log("api",apiUrl);
-
 const api = axios.create({
   baseURL: apiUrl, 
 });
