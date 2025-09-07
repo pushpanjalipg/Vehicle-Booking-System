437a87ca7d9ac7b0484c317938b2ff1818251678
pushpanjalipg <pushpanjalipg700@gmail.com>
2025-09-07 22:22:13 +0530

    after build

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom", // 👈 vitest ko browser-like environment milega
+    environment: "jsdom", // 
     globals: true,
     setupFiles: "./src/setupTests.js",
   },
