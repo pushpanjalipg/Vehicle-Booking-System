74e83508845a9a841177de319807ada0ee45aa54
pushpanjalipg <pushpanjalipg700@gmail.com>
2025-09-07 23:33:11 +0530

    after use vite place of react script

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const apiUrl = process.env.REACT_APP_API_URL;
+// const apiUrl = process.env.REACT_APP_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL;
 const api = axios.create({
   baseURL: apiUrl, 
 });
